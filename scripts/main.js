42824ba6d68a9ea6ad2e7294ea5d9ada41f21a57
kolaso13 <72077646+kolaso13@users.noreply.github.com>
2021-10-14 09:10:20 +0200

    PaginaConVersionOscura

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
-document.addEventListener('click',function(e){
+// @see https://codepen.io/typo3-freelancer/pen/poEvyGj
+
+document.addEventListener('click',(ev) => {
     // Hamburger menu
-    if(e.target.classList.contains('hamburger-toggle')){
-        e.target.children[0].classList.toggle('active');
+    if (ev.target.classList.contains('hamburger-toggle')) {
+        ev.target.children[0].classList.toggle('active');
     }
-})
+});
